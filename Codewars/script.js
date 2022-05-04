b8706378fba7f7fabdef36b88d90d62bc0311682
Bryce Stabenow <brycestabenow617@gmail.com>
2022-05-04 13:11:06 -0700

    another cw for todat

--- Codewars/script.js
+++ Codewars/script.js
@@ -824,3 +824,20 @@ function correct(string) {
 		})
 		.join('');
 }
+
+function correct(string) {
+	return string
+		.split('')
+		.map((x) => {
+			if (x === '5') {
+				return 'S';
+			} else if (x === '0') {
+				return 'O';
+			} else if (x === '1') {
+				return 'I';
+			} else {
+				return x;
+			}
+		})
+		.join('');
+}
