3f6eb48150efbe99ad1e9dd1e92fa474c338d70d
Bryce Stabenow <brycestabenow617@gmail.com>
2022-05-06 10:41:07 -0700

    codewars for 5/6

--- Codewars/script.js
+++ Codewars/script.js
@@ -868,14 +868,18 @@ function sumStr(a, b) {
 	return (Number(a) + Number(b)).toString();
 }
 
-function diamond (n) {
-  if (n <= 0 || n % 2 === 0) return null
-  str = ''
-  for (let i = 0; i < n; i++) { 
-    let len = Math.abs((n-2*i-1)/2)
-    str += ' '.repeat(len)
-    str += '*'.repeat(n-2*len)
-    str += '\n'
-  }
-  return str
+function diamond(n) {
+	if (n <= 0 || n % 2 === 0) return null;
+	str = '';
+	for (let i = 0; i < n; i++) {
+		let len = Math.abs((n - 2 * i - 1) / 2);
+		str += ' '.repeat(len);
+		str += '*'.repeat(n - 2 * len);
+		str += '\n';
+	}
+	return str;
+}
+
+function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+	return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
 }
