7d0cec0ec23763e2587123800c2e14de88b77d49
Bryce Stabenow <brycestabenow617@gmail.com>
2022-05-06 10:44:33 -0700

    another cw for 5/6

--- Codewars/script.js
+++ Codewars/script.js
@@ -883,3 +883,12 @@ function diamond(n) {
 function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
 	return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
 }
+
+function nbDig(n, d) {
+	var res = 0;
+	for (var g = 0; g <= n; g++) {
+		var square = (g * g + '').split('');
+		square.forEach((s) => (s == d ? res++ : null));
+	}
+	return res;
+}
