964f7fcbe03f826df335c2170280a8bf7d0c75c7
Bryce Stabenow <brycestabenow617@gmail.com>
2022-06-01 17:50:47 -0700

    new cw for strings and arrays in PHP

--- Codewars/codewars.php
+++ Codewars/codewars.php
@@ -1,3 +1,5 @@
+<?php
+
 function hello($name = ''): string {
   //Edge cases where name is not given
   if($name == '' || $name == NULL){
@@ -12,4 +14,17 @@ function hello($name = ''): string {
 
 function otherAngle($a, $b) {
   return 180 - $a - $b;
+}
+
+function stairsIn20($arr) {
+  return 20 * (array_sum($arr[0]) + array_sum($arr[1]) + array_sum($arr[2]) + array_sum($arr[3]) + array_sum($arr[4]) + array_sum($arr[5]) + array_sum($arr[6]));
+}
+
+function replace(string $s): string {
+  $vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+  
+  /*
+  str_replace( (char to be replaced), (char to insert), string, count)
+  */
+  return str_replace($vowels, '!', $s);
 }
