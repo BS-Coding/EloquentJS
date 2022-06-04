3e33e8b5aac1107188d32db36b30ced5ce93344c
Bryce Stabenow <brycestabenow617@gmail.com>
2022-06-04 14:41:21 -0700

    new cw

--- Codewars/codewars.php
+++ Codewars/codewars.php
@@ -132,3 +132,12 @@ function f($n=0) {
     return false;
   }
 }
+function series_sum($n) {
+  $sum = 0;
+  
+  for ($i = 0; $i <= ($n - 1); $i++) {
+    $sum += (1 / (1 + (3*$i)));
+  }
+  
+  return number_format($sum, 2, '.', '');
+}
